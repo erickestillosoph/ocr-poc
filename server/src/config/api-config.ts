7271314e2ba136ea71f7e71fcf8630e49da797f2
Erick Estilloso <revestilloso@gmail.com>
2025-04-14 10:14:30 +0800

    fix for api route at server

--- server/src/config/api-config.ts
+++ server/src/config/api-config.ts
@@ -3,7 +3,11 @@ export const API_PREFIXES = ["/api/v1"];
 // Define environment
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const isProduction = NODE_ENV === "production";
-export const environment = isProduction ? "production" : "local";
+export const environment = process.env.VERCEL
+  ? "production"
+  : isProduction
+  ? "production"
+  : "local";
 export const API_VERSION = process.env.API_VERSION || "";
 export const API_KEY = process.env.API_KEY || "dev-api-key-123";
 
