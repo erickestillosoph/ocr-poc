b1bf009fb9a1845f241a29cfa207a5c4093c1487
Erick Estilloso <revestilloso@gmail.com>
2025-04-14 23:01:46 +0800

    added mutation and camera functionality and no more tabs for view results

--- client/src/pages/view-results-page/ui/view-results-page-ui.tsx
+++ client/src/pages/view-results-page/ui/view-results-page-ui.tsx
@@ -1,79 +1,76 @@
 import { useAppTheme } from "@/shared/theme";
-import {
-  Code,
-  VStack,
-  Tabs,
-  TabList,
-  TabPanels,
-  Tab,
-  TabPanel,
-} from "@chakra-ui/react";
+import { Code, VStack, Text, Box, Spacer } from "@chakra-ui/react";
 import { getLocalStorage } from "@/shared";
 
 export const ViewResultsPageUI = () => {
   const { theme } = useAppTheme();
   const imageResults = getLocalStorage("imageResults");
   const pdfResults = getLocalStorage("pdfResults");
+  const cameraImageResults = getLocalStorage("cameraImageResults");
 
   const imageResultsArray = JSON.stringify(imageResults, null, 1);
   const pdfResultsArray = JSON.stringify(pdfResults, null, 1);
+  const cameraImageResultsArray = JSON.stringify(cameraImageResults, null, 1);
 
+  const titleCaption = (title: string, json: string) => {
+    return (
+      <VStack>
+        <Text
+          fontSize="lg"
+          display="flex"
+          color={theme.colors.blue}
+          textAlign="left"
+          textTransform="uppercase"
+          fontWeight="bold"
+          w="full"
+        >
+          <Text
+            p="12px 20px"
+            borderRadius="10px"
+            backgroundColor={theme.colors.gray[200]}
+          >
+            {title}
+          </Text>
+        </Text>
+        <Box
+          backgroundColor={theme.colors.gray[100]}
+          borderRadius="10px"
+          p="20px 20px"
+        >
+          <Code
+            fontSize="sm"
+            w="full"
+            textAlign="left"
+            appearance="menulist"
+            variant="subtle"
+            overflow="auto"
+            maxH="50vh"
+            backgroundColor="transparent"
+          >
+            {json}
+          </Code>
+        </Box>
+      </VStack>
+    );
+  };
   return (
     <VStack
       mt="20px"
       width="100%"
-      display="grid"
+      display="flex"
+      flexDirection="column"
+      gap="20px"
       alignContent="space-between"
       height="full"
       w="full"
+      mb="120px"
     >
-      <Tabs
-        variant="soft-rounded"
-        size="sm"
-        w="full"
-        colorScheme={theme.colors.blue[100]}
-      >
-        <TabList gap={2}>
-          <Tab>Image</Tab>
-          <Tab>Pdf</Tab>
-        </TabList>
-
-        <TabPanels>
-          {imageResults && (
-            <TabPanel>
-              <Code
-                backgroundColor={theme.colors.gray[50]}
-                fontSize="sm"
-                w="full"
-                textAlign="left"
-                appearance="auto"
-                variant="subtle"
-                overflow="auto"
-                maxH="50vh"
-              >
-                {imageResultsArray}
-              </Code>
-            </TabPanel>
-          )}
-
-          {pdfResults && (
-            <TabPanel>
-              <Code
-                backgroundColor={theme.colors.gray[50]}
-                fontSize="sm"
-                w="full"
-                textAlign="left"
-                appearance="auto"
-                variant="subtle"
-                overflow="auto"
-                maxH="50vh"
-              >
-                {pdfResultsArray}
-              </Code>
-            </TabPanel>
-          )}
-        </TabPanels>
-      </Tabs>
+      <Spacer padding="30px" />
+      {imageResults && titleCaption("Image Results", imageResultsArray)}
+      {pdfResults && titleCaption("Pdf Results", pdfResultsArray)}
+      {cameraImageResults &&
+        titleCaption("Camera Image Results", cameraImageResultsArray)}
+      <Spacer padding="30px" />
     </VStack>
   );
 };
