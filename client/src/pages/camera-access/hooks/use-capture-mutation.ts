b20ea6bde25a48f4a92b8b3cfb3ed187e83392f1
Erick Estilloso <revestilloso@gmail.com>
2025-04-15 17:12:18 +0800

    camera back base 64 to image file and added new mutation using image api path

---
+++ client/src/pages/camera-access/hooks/use-capture-mutation.ts
@@ -0,0 +1,95 @@
+import { useMutation } from "@tanstack/react-query";
+import { paths, setLocalStorage } from "@/shared";
+import { useDropzone } from "react-dropzone";
+import { useToast } from "@chakra-ui/react";
+import { useCallback, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { apiClient, apiPaths } from "@/shared/config/api-config";
+
+export const useImageCaptureMutation = () => {
+  const toast = useToast();
+  const navigate = useNavigate();
+  const IMAGE_PATH = apiPaths.uploadImage;
+
+  const openImageRef = useRef<() => void | null>(null);
+
+  const {
+    mutateAsync,
+    isPending,
+    isError,
+    isSuccess,
+    data: imageData,
+  } = useMutation({
+    mutationFn: (files: File[]) => {
+      const data = new FormData();
+      files.forEach((file) => data.append("image", file));
+      return apiClient.post(IMAGE_PATH, data);
+    },
+    onSuccess: (data) => {
+      setLocalStorage("cameraImageResultsTimestamp", Date.now());
+      setLocalStorage("cameraImageResults", data.data);
+      toast({
+        title: "Upload successful",
+        description: `Your image has been uploaded successfully.`,
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+        position: "top",
+      });
+      navigate(paths.viewResultsPage);
+    },
+    onError: (error) => {
+      console.error("Upload error:", error);
+      toast({
+        title: "Upload failed",
+        description: `There was an error uploading your image.`,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+        position: "top",
+      });
+    },
+    mutationKey: ["image"],
+  });
+
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      acceptedFiles.forEach((file) => {
+        toast({
+          title: "File uploaded",
+          description: file.name,
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+          position: "top",
+        });
+      });
+
+      mutateAsync(acceptedFiles);
+    },
+    [toast, mutateAsync]
+  );
+
+  const { getRootProps, getInputProps, open } = useDropzone({
+    onDrop,
+    accept: {
+      "image/jpeg": [],
+      "image/png": [],
+      "image/jpg": [],
+    },
+    maxFiles: 3,
+  });
+
+  openImageRef.current = open;
+
+  return {
+    getRootProps,
+    getInputProps,
+    mutate: mutateAsync,
+    isPending,
+    isError,
+    isSuccess,
+    openImageRef,
+    imageResults: imageData?.data,
+  };
+};
