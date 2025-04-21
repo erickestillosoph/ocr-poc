dc6a3c12ca5bb946d9ae0f9043acdc8955adb35d
Erick Estilloso <revestilloso@gmail.com>
2025-04-21 10:03:20 +0800

    develop branch

--- client/src/pages/upload-page/hooks/file-base-64.ts
+++ client/src/pages/upload-page/hooks/file-base-64.ts
@@ -1,3 +1,8 @@
+//TODO: Please filter out if it is an image or pdf the return string of base64 conversion has a application/pdf;base64 mime type or image/jpeg;base64 mime type
+// TODO: apply on files for camera access on files below
+// use-camera-access.ts and  use-image-capture-mutation.ts
+// use-pdf-mutation.ts
+// use-image-mutation.ts
 export const fileToBase64 = (file: File): Promise<string> => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
